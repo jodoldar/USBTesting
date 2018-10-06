e3cbfe36ae47a5e5bb9d6729cde7a231b9a5220a
Josep <jodoldar@gmail.com>
2018-10-06 18:42:17 +0200

    Added getter methods

--- Observation.cpp
+++ Observation.cpp
@@ -11,4 +11,33 @@ class Observation
         float wind_gust[];
         int wind_dir[];
         float rainfall;
+
+    public:
+        double getTimestamp() {
+            return timestamp;
+        }
+
+        float getTemperature(int pos) {
+            return temperature[pos];
+        }
+
+        float getHumidity(int pos) {
+            return humidity[pos];
+        }
+
+        float getPressure() {
+            return pressure;
+        }
+
+        float getWindGust(int pos) {
+            return wind_gust[pos];
+        }
+
+        int getWindDir(int pos) {
+            return wind_dir[pos];
+        }
+
+        float getRainfall() {
+            return rainfall;
+        }
 };
